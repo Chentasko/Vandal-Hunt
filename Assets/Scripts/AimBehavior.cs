2401f1b464993775acdc5144e9873b9f7ec581f6
Chentasko <155901522+Chentasko@users.noreply.github.com>
2024-06-03 20:44:30 +0300

    v0.0.4 (pre-alpha)
    -Pointer has been locked to aim cursor
    -Cursor has been adjusted to be hidden in-game and not go out of the game window
    -First colliders added

--- Assets/Scripts/AimBehavior.cs
+++ Assets/Scripts/AimBehavior.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class AimBehavior : MonoBehaviour
 {
-    //Vector2 aimPos;
+    Vector2 aimPos; //niþan
+    Vector2 mousPos; //fare 
     
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 aimPos = Input.mousePosition;
+        GetComponent<Transform>().position = aimPos;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
     }
-
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
+        Cursor.visible = false;
+        Debug.Log(aimPos);
+        mousPos = Input.mousePosition;
+        aimPos = Camera.main.ScreenToWorldPoint(mousPos);
+        transform.position = aimPos;
+
+
+
         //Transform.Position.x = aimPos.x; 
         //Transform.Position.x = aimPos.y;
     }
