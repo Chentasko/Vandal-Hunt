6d0b21059b21ecfaf613e610b2e247ca04a2d3fb
Chentasko <155901522+Chentasko@users.noreply.github.com>
2024-06-04 13:57:55 +0300

    v.0.0.5 (pre-alpha)
    Shooting logic is being prepared. Currently undone. Will be worked on further in the next update. IEnumerators will be used instead of Void functions for the shooting penalty

--- Assets/Scripts/AimBehavior.cs
+++ Assets/Scripts/AimBehavior.cs
@@ -8,24 +8,66 @@ public class AimBehavior : MonoBehaviour
 {
     Vector2 aimPos; //niþan
     Vector2 mousPos; //fare 
+    bool isFreeToShoot = true;
+    bool shootTrigger = false;
+    float loadTime = 3;
     
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Transform>().position = aimPos;
+        GetComponent<Collider2D>().isTrigger = shootTrigger;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
     }
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShootingPenalty()
+    {
+        isFreeToShoot = false;
+        Debug.Log("I am WAITING");
+        yield return new WaitForSeconds(10f);
+        Debug.Log("I am NOW DONE WAITING");
+        
+    }
+
+    void ShootLogic() //Doðru çalýþmýyor burayý yeniden yap. Void ve ifler yerine hepsini bir IEnumerator içinde kullanmayý dene (
+    {
+        if (Input.GetMouseButtonDown(0) && isFreeToShoot) 
+        {
+
+            Debug.Log("SHOOT SHOOT BAM BAM");
+            shootTrigger = true;
+            //isFreeToShoot = false;
+            //ses kodu gelecek buraya
+            
+
+        } else if (!isFreeToShoot)
+        {
+            shootTrigger = false;
+            Debug.Log("i cant shoot right now PENALTY");
+        
+        }
+        else 
+        {
+            
+        }   
+    }
+
+    void CursorLogic() 
     {
         Cursor.visible = false;
-        Debug.Log(aimPos);
+        //Debug.Log(aimPos);
         mousPos = Input.mousePosition;
         aimPos = Camera.main.ScreenToWorldPoint(mousPos);
         transform.position = aimPos;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        CursorLogic();
+        ShootLogic();
+        
 
 
         //Transform.Position.x = aimPos.x; 
---
+++ Assets/Scripts/CustomerBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void CustomerAI() 
+    { 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
