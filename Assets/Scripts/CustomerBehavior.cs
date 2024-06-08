cf5e2d22d67d4daa07db40f637c13d789e4b3535
Chentasko <155901522+Chentasko@users.noreply.github.com>
2024-06-09 01:46:47 +0300

    v0.0.85 (pre-alpha)
    Grid script made (currently empty)
    Variables of the customer behaviour script added

--- Assets/Scripts/CustomerBehavior.cs
+++ Assets/Scripts/CustomerBehavior.cs
@@ -1,15 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomerBehavior : MonoBehaviour
 {
+    [SerializeField] private float criminalChance;
+    [SerializeField] private bool criminal = false;
+    [SerializeField] private bool carry = false;
+    [SerializeField] float speed = 20;
+    [SerializeField] float distance = 0; //use????
+
+    public enum custState
+    {
+        Idle,
+        Moving,
+        Picking,
+        Dead,
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void PathfindTEST() 
+    { 
+        
+    
+    }
     void CustomerAI() 
     { 
     
